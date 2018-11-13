31c6162371370cf5c016d6623d7f1bd2e307cdb1
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-13 09:14:28 +0100

    Update prints.py

--- plugins/prints.py
+++ plugins/prints.py
@@ -1,16 +1,18 @@
 import config
 import urllib
 import dotenv
+from dotenv import load_dotenv
+# OR, the same with increased verbosity:
+load_dotenv(verbose=True)
 import os
 bot = config.bot
+papi = 'abdf158215d05e1a973510c5c81d9a2cdd99ad2c7cd8'
 
 def prints(msg):
     if msg.get('text'):
         if msg['text'].startswith('/print ') or msg['text'].startswith('!print '):
             try:
-            	apiurl = 'https://api.thumbnail.ws/api'
-    			papi = 'abdf158215d05e1a973510c5c81d9a2cdd99ad2c7cd8'
-                bot.sendPhoto(msg['chat']['id'], f"{apiurl}/{papi}/thumbnail/get?url={urllib.parse.quote_plus(msg['text'][7:])}&width=1280",
+                bot.sendPhoto(msg['chat']['id'], f"https://api.thumbnail.ws/api/{papi}/thumbnail/get?url={urllib.parse.quote_plus(msg['text'][7:])}&width=1280",
                               reply_to_message_id=msg['message_id'])
             except Exception as e:
                 bot.sendMessage(msg['chat']['id'], f'Ocorreu um erro ao enviar a print, favor tente mais tarde.\nDescrição do erro: {e.description}',
