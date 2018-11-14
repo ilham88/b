8d0c6ec3d6048aff9ff3db8e3696dd7265cdb0e0
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-14 12:03:06 +0100

    Update prints.py

--- plugins/prints.py
+++ plugins/prints.py
@@ -55,13 +55,10 @@ def prints(msg):
                 ms = (end - start).seconds
                 f = "searched Google for {} in {} seconds. \n\n{}".format(input_str, ms, x)
                 
-                bot.sendMessage(msg['chat']['id'], f, 'Markdown',
+                last_search = bot.sendMessage(msg['chat']['id'], f, 'Markdown',
                                 reply_to_message_id=msg['message_id'])
                 try:
-                    bot.sendLocation(msg['chat']['id'],
-                                     latitude=req['lat'],
-                                     longitude=req['lon'],
-                                     reply_to_message_id=msg['message_id'], disable_web_page_preview=True)
+                    bot.deleteMessage((msg['chat']['id'],last_search)
                 except KeyError:
                     pass
-                
+               
