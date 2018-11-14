d62d18dc758857ac1b3bd76f4068a5f8ec683838
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-14 09:54:45 +0100

    Update prints.py

--- plugins/prints.py
+++ plugins/prints.py
@@ -18,9 +18,14 @@ papi = os.environ["screenshots"]
 def prints(msg):
     if msg.get('text'):
         if msg['text'].startswith('/print ') or msg['text'].startswith('!print '):
-            if msg['text'][7:] = '':
-                bot.sendMessage(msg['chat']['id'], 'Use: /tr <lang> text to translate (can also be used when replying to a message).',
+            text = msg['text'][6:]
+
+            if text == '':
+                bot.sendMessage(msg['chat']['id'], '*Uso:* /ytdl URL do vídeo ou nome', 'Markdown',
                                 reply_to_message_id=msg['message_id'])
+            else:
+                sent_id = bot.sendMessage(msg['chat']['id'], 'Obtendo informações do vídeo...', 'Markdown',
+                                          reply_to_message_id=msg['message_id'])['message_id']
                 try:
                     bot.sendPhoto(msg['chat']['id'], f"https://api.thumbnail.ws/api/{papi}/thumbnail/get?url={urllib.parse.quote_plus(msg['text'][7:])}&width=1280",
                               reply_to_message_id=msg['message_id'])
