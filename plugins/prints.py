ce45452273a9f85fb04738dc75eeb9bc831aaacc
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-14 11:40:48 +0100

    Update prints.py

--- plugins/prints.py
+++ plugins/prints.py
@@ -42,20 +42,19 @@ def prints(msg):
         if msg['text'].startswith('/g') or msg['text'].startswith('!g'):
             input_str = msg['text'][3:]
             if input_str == '':
-                bot.sendMessage(msg['chat']['id'], '*Uso:* `/ip IP/endereço`',
+                bot.sendMessage(msg['chat']['id'], '*Use:* `/g or !g <search query>`',
                                 parse_mode='Markdown',
                                 reply_to_message_id=msg['message_id'])
             else:
-                if msg.get('reply_to_message') and msg['reply_to_message'].get('text'):
-                    rep = msg['reply_to_message'].get('text')
-                    start = datetime.now()
-                    req = search(input_str, num_results=GLOBAL_LIMIT)
-                    x = " "
-                    for text, url in req:
-                        x += "  🔎 [{}]({}) \n\n".format(text, url)
+      
+                start = datetime.now()
+                req = search(input_str, num_results=GLOBAL_LIMIT)
+                x = " "
+                for text, url in req:
+                    x += "  🔎 [{}]({}) \n\n".format(text, url)
                
-                    end = datetime.now()
-                    ms = (end - start).seconds
-                    f = "searched Google for {} in {} seconds. \n\n{}".format(input_str, ms, x)
-                    bot.sendMessage(msg['chat']['id'], f, 'Markdown',
+                end = datetime.now()
+                ms = (end - start).seconds
+                f = "searched Google for {} in {} seconds. \n\n{}".format(input_str, ms, x)
+                bot.sendMessage(msg['chat']['id'], f, 'Markdown',
                                 reply_to_message_id=msg['message_id'], disable_web_page_preview=True)
