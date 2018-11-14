8e8bf5a887d61fbd870ec3cfc3e71c960a6aabb3
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-14 16:24:30 +0100

    Update dados.py

--- plugins/dados.py
+++ plugins/dados.py
@@ -1,14 +1,39 @@
 import config
-import random
+import requests
+import re
 
+def get_filename_from_cd(cd):
+    """
+    Get filename from content-disposition
+    """
+    if not cd:
+        return None
+    fname = re.findall('filename=(.+)', cd)
+    if len(fname) == 0:
+        return None
+    return fname[0]
 bot = config.bot
 bot_username = config.bot_username
 
 
-def dados(msg):
+def prints(msg):
     if msg.get('text'):
-        if msg['text'] == '/dados' or msg['text'] == '!dados' or msg['text'] == '/dados@' + bot_username:
-            dados = random.randint(1, 6)
-            bot.sendMessage(msg['chat']['id'], '🎲 O dado parou no número: {}'.format(dados),
-                            reply_to_message_id=msg['message_id'])
-            return True
+        if msg['text'].startswith('/dl') or msg['text'].startswith('!dl'):
+            input_str = msg['text'][3:]
+            if input_str == '':
+                bot.sendMessage(msg['chat']['id'], '*Use:* `/dl or !dl <url/link>`',
+                                parse_mode='Markdown',
+                                reply_to_message_id=msg['message_id'])
+            else:
+                sent = bot.sendMessage(msg['chat']['id'], '*Processing your request..... 🔁*', 'Markdown', reply_to_message_id=msg['message_id'])[
+                'message_id']
+                start = datetime.now()
+                url = input_str
+                r = requests.get(url, allow_redirects=True)filename = get_filename_from_cd(r.headers.get('content-disposition'))
+                f = open(filename, 'wb').write(r.content)
+                j = f.json
+                print(j)
+                end = datetime.now()
+                ms = (end - start).seconds
+                bot.editMessageText((msg['chat']['id'], sent), "searched Google", 'Markdown', disable_web_page_preview=True)
+                return True
