ffe0edfbf89471b1e0a92363c4440c8269366628
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-14 09:22:00 +0100

    Update github.py

--- plugins/github.py
+++ plugins/github.py
@@ -16,6 +16,39 @@ def github(msg):
             else:
                 url = 'https://api.github.com/users/{}'.format(msg['text'][6:])
                 j = requests.get(url)
+                if j.status_code = 404:
+                    res = "`{}`: {}".format(msg['text'][6:], r.text)
+                else:
+                    if j.status_code != 404:
+                        b = j.json()
+                        print(b)
+                        avatar_url = b["avatar_url"]
+                        html_url = b["html_url"]
+                        gh_type = b["type"]
+                        name = b["name"]
+                        company = b["company"]
+                        blog = b["blog"]
+                        location = b["location"]
+                        bio = b["bio"]
+                        created_at = b["created_at"]
+                        res = """[\u2063]({})Name: [{}]({})
+Type: {}
+Company: {}
+Blog: {}
+Location: {}
+Bio: {}
+Profile Created: {}""".format(avatar_url, name, html_url, gh_type, company, blog, location, bio, created_at)
+                bot.sendMessage(msg['chat']['id'], res, 'Markdown', reply_to_message_id=msg['message_id'])
+                return True
+
+def github(msg):
+    if msg.get('text'):
+        if msg['text'].startswith('/gityh'):
+            if msg['text'][6:] == '':
+                res = '*Uso:* `/gith <cidade>` - _Obtem informações meteorológicas da cidade._'
+            else:
+                url = 'https://api.github.com/users/{}'.format(msg['text'][6:])
+                j = requests.get(url)
                 if j.status_code != 404:
                     b = j.json()
                     print(b)
@@ -37,4 +70,3 @@ Bio: {}
 Profile Created: {}""".format(avatar_url, name, html_url, gh_type, company, blog, location, bio, created_at)
             bot.sendMessage(msg['chat']['id'], res, 'Markdown', reply_to_message_id=msg['message_id'])
             return True
-
