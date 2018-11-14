a918c03da5c64a608ce21539e173a9dba026fc44
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-14 09:50:25 +0100

    Update prints.py

--- plugins/prints.py
+++ plugins/prints.py
@@ -18,13 +18,14 @@ papi = os.environ["screenshots"]
 def prints(msg):
     if msg.get('text'):
         if msg['text'].startswith('/print ') or msg['text'].startswith('!print '):
-            try:
-                bot.sendPhoto(msg['chat']['id'], f"https://api.thumbnail.ws/api/{papi}/thumbnail/get?url={urllib.parse.quote_plus(msg['text'][7:])}&width=1280",
-                              reply_to_message_id=msg['message_id'])
-            except Exception as e:
-                bot.sendMessage(msg['chat']['id'], f'Ocorreu um erro ao enviar a print, favor tente mais tarde.\nDescrição do erro: {e.description}',
-                                reply_to_message_id=msg['message_id'])
-            else:
+            if msg['text'][7:] = ''
                 bot.sendMessage(msg['chat']['id'], 'Use: /tr <lang> text to translate (can also be used when replying to a message).',
                                 reply_to_message_id=msg['message_id'])
-            return True
+                try:
+                    bot.sendPhoto(msg['chat']['id'], f"https://api.thumbnail.ws/api/{papi}/thumbnail/get?url={urllib.parse.quote_plus(msg['text'][7:])}&width=1280",
+                              reply_to_message_id=msg['message_id'])
+                except Exception as e:
+                    bot.sendMessage(msg['chat']['id'], f'Ocorreu um erro ao enviar a print, favor tente mais tarde.\nDescrição do erro: {e.description}',
+                                reply_to_message_id=msg['message_id'])
+            
+                return True
