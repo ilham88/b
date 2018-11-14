1a62b7c8fe4020946a9a22c414e6a75472f706de
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-14 08:35:50 +0100

    Update github.py

--- plugins/github.py
+++ plugins/github.py
@@ -4,33 +4,35 @@ import dotenv
 import os
 
 
-def github(msg):
+
+url = 'https://api.github.com/users/{}'
+
+OFFSET = 127462 - ord('A')
+
+
+def flag(code):
+    return chr(ord(code[0]) + OFFSET) + chr(ord(code[1]) + OFFSET)
+
+
+def clima(msg):
     if msg.get('text'):
-        if msg['text'].startswith('/gith ') or msg['text'].startswith('!gith '):
-          text = msg['text'][6:]
-          try:
-            url = requests.get('https://api.github.com/users/{}".format(msg['text'][6:])
-            r = requests.get(url)
-            if r.status_code != 404:
-                b = r.json()
-                avatar_url = b["avatar_url"]
-                html_url = b["html_url"]
-                gh_type = b["type"]
-                name = b["name"]
-                company = b["company"]
-                blog = b["blog"]
-                location = b["location"]
-                bio = b["bio"]
-                created_at = b["created_at"]
-                sent_id = bot.sendMessage(msg['chat']['id'], 'Obtendo informações do vídeo...', 'Markdown',
-                                          reply_to_message_id=msg['message_id'])['message_id']
-                bot.editMessageText((msg['chat']['id'],sent_id),
-                                        """[\u2063]({})Name: [{}]({})
+        if msg['text'].startswith('/gith'):
+            if msg['text'][6:] == '':
+                res = '*Uso:* `/gith <cidade>` - _Obtem informações meteorológicas da cidade._'
+            else:
+                json = requests.post(url.format(msg['text'][6:])).json()
+                if json['status_code'] != 404:
+                    print(json)
+                    res = json['message']
+                else:
+                    res = """[\u2063]({})Name: [{}]({})
 Type: {}
 Company: {}
 Blog: {}
 Location: {}
 Bio: {}
-Profile Created: {}""".format(avatar_url, name, html_url, gh_type, company, blog, location, bio, created_at), 'Markdown')
-                else:
-                bot.editMessageText((msg['chat']['id'],sent_id), "`{}`: {}".format(input_str, r.text), 'Markdown')
+Profile Created: {}""".format(json['avatar_url'], json['name'], json['html_url']['gh_type'],
+                              json['company']['blog'], json['location'], json['bio'], json['created_at'])
+            bot.sendMessage(msg['chat']['id'], res, 'Markdown', reply_to_message_id=msg['message_id'])
+            return True
+
