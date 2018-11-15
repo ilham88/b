04ab6083818a0e47853589d1b637e567cc96724f
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-15 02:34:16 +0100

    Update dados.py

--- plugins/dados.py
+++ plugins/dados.py
@@ -8,81 +8,86 @@ try:
 except ImportError:
     import urllib2
     python3 = False
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function
+from bs4 import BeautifulSoup
+import progressbar
+import requests
+import sys
+### XXX: hack to skip some stupid beautifulsoup warnings that I'll fix when refactoring
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
-def get_filename_from_cd(cd):
-    """
-    Get filename from content-disposition
-    """
-    if not cd:
-        return None
-    fname = re.findall('filename=(.+)', cd)
-    if len(fname) == 0:
-        return None
-    return fname[0]
-def progress_callback_simple(downloaded,total):
-    sys.stdout.write(
-        "\r" +
-        (len(str(total))-len(str(downloaded)))*" " + str(downloaded) + "/%d"%total +
-        " [%3.2f%%]"%(100.0*float(downloaded)/float(total))
-    )
-    sys.stdout.flush()
+def get_apk(app_name): # XXX: this function needs refactoring/cleaning.
+    print("{+} getting download link for %s" %(app_name))
+    site = "https://apkpure.com"
+    url = "https://apkpure.com/search?q=%s" %(app_name)
+    html = requests.get(url)
+    parse = BeautifulSoup(html.text)
+    for i in parse.find("p"):
+        a_url = i["href"]
+        app_url = site + a_url + "/download?from=details"
+        html2 = requests.get(app_url)
+        parse2 = BeautifulSoup(html2.text)
+        for link in parse2.find_all("a",id="download_link"):
+            download_link = link["href"]
+        download_apk(app_name, download_link)
 
-def download(srcurl, dstfilepath, progress_callback=None, block_size=8192):
-    def _download_helper(response, out_file, file_size):
-        if progress_callback!=None: progress_callback(0,file_size)
-        if block_size == None:
-            buffer = response.read()
-            out_file.write(buffer)
+def make_progress_bar():
+    return progressbar.ProgressBar(
+        redirect_stdout=True,
+        redirect_stderr=True,
+        widgets=[
+            progressbar.Percentage(),
+            progressbar.Bar(),
+            ' (',
+            progressbar.AdaptiveTransferSpeed(),
+            ' ',
+            progressbar.ETA(),
+            ') ',
+        ])
 
-            if progress_callback!=None: progress_callback(file_size,file_size)
-        else:
-            file_size_dl = 0
-            while True:
-                buffer = response.read(block_size)
-                if not buffer: break
+def download_apk(app_name, download_link):
+    print("{+} downloading %s" %(app_name))
+    output_file = "output/" + app_name + ".apk"
+    r = requests.get(url=download_link, stream=True)
+    with open(output_file, 'wb') as f:
+        total_length = int(r.headers.get('content-length'))
+        bar = make_progress_bar()
+        bar.start(total_length)
+        readsofar = 0
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                readsofar += len(chunk)
+                bar.update(readsofar)
+                f.write(chunk)
+                f.flush()
+        bar.finish()
+    print("{+} done. file saved to %s" %(output_file))
 
-                file_size_dl += len(buffer)
-                out_file.write(buffer)
-
-                if progress_callback!=None: progress_callback(file_size_dl,file_size)
-               
-    with open(dstfilepath,"wb") as out_file:
-        if python3:
-            with urllib.request.urlopen(srcurl) as response:
-                file_size = int(response.getheader("Content-Length"))
-                _download_helper(response,out_file,file_size)
-        else:
-            response = urllib2.urlopen(srcurl)
-            meta = response.info()
-            file_size = int(meta.getheaders("Content-Length")[0])
-            _download_helper(response,out_file,file_size)
-
-import traceback
-try:
-    def dados(msg):
-        if msg.get('text'):
-            if msg['text'].startswith('/dl') or msg['text'].startswith('!dl'):
-                input_str = msg['text'][3:]
-                if input_str == '':
-                    bot.sendMessage(msg['chat']['id'], '*Use:* `/dl or !dl <url/link>`',
+def dados(msg):
+    if msg.get('text'):
+        if msg['text'].startswith('/dl') or msg['text'].startswith('!dl'):
+            input_str = msg['text'][3:]
+            if input_str == '':
+                bot.sendMessage(msg['chat']['id'], '*Use:* `/dl or !dl <url/link>`',
                                 parse_mode='Markdown',
                                 reply_to_message_id=msg['message_id'])
-                else:
-                    sent = bot.sendMessage(msg['chat']['id'], "searched Google........", 'Markdown', reply_to_message_id=msg['message_id'])['message_id']
-                    url = input_str
-                    filename = url.split('/')[-1]
-                    with open(filename,"wb") as out_file:
-                        with urllib.request.urlopen(url) as response:
-                        file_size = int(response.getheader("Content-Length"))
-                        _download_helper(response,out_file,file_size)
-                        file = download(url, filename, progress_callback_simple)
-                        bot.sendChatAction(msg['chat']['id'], 'upload_document')
-                        bot.sendDocument(msg['chat']['id'], file, reply_to_message_id=msg['message_id'])
-                        bot.editMessageText((msg['chat']['id'], sent), "searched Google", 'Markdown', disable_web_page_preview=True)
-                        return True    
-except:
-    traceback.print_exc()
-    input()
+            else:
+                sent = bot.sendMessage(msg['chat']['id'], "searched Google........", 'Markdown', reply_to_message_id=msg['message_id'])['message_id']
+                bot.sendChatAction(msg['chat']['id'], 'upload_document')
+                bot.sendDocument(msg['chat']['id'], output_file, reply_to_message_id=msg['message_id'])
+                bot.editMessageText((msg['chat']['id'], sent), "done. file savede", 'Markdown', disable_web_page_preview=True)
+                return True     
     
+def main(args):
+    if len(args) != 2:
+        sys.exit("use: %s com.blah.blah" %(args[0]))
+    get_apk(args[1])
+
+if __name__ == "__main__":
+    main(args=sys.argv)
+
 
                 
