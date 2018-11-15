45f2c327928198c36b776cedb3f847b75024ae53
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-15 21:46:58 +0100

    Update dados.py

--- plugins/dados.py
+++ plugins/dados.py
@@ -84,7 +84,32 @@ def examine(result, type):
         answer = input('Do you want to continue? [y] ')
         if answer != 'y':
             exit(1)
-
+def progress_callback_simple(downloaded,total):
+        sys.stdout.write(
+            "\r" +
+            (len(str(total))-len(str(downloaded)))*" " + str(downloaded) + "/%d"%total +
+            " [%3.2f%%]"%(100.0*float(downloaded)/float(total))
+        )
+        sys.stdout.flush()
+    
+    def download(srcurl, dstfilepath, progress_callback=None, block_size=8192):
+        def _download_helper(response, out_file, file_size):
+            if progress_callback!=None: progress_callback(0,file_size)
+            if block_size == None:
+                buffer = response.read()
+                out_file.write(buffer)
+                
+                if progress_callback!=None: progress_callback(file_size,file_size)
+            else:
+                file_size_dl = 0
+                while True:
+                    buffer = response.read(block_size)
+                    if not buffer: break
+    
+                    file_size_dl += len(buffer)
+                    out_file.write(buffer)
+    
+                    if progress_callback!=None: progress_callback(file_size_dl,file_size)
 def dados(msg):
     content_type, chat_type, chat_id, msg_date, msg_id = amanobot.glance(msg, long=True)
     if msg.get('text'):
