c6cf4195cda782ddb9bd47e6bfddc2b527476046
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-16 13:23:51 +0100

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -2,37 +2,37 @@ from amanobot.namedtuple import InlineKeyboardMarkup
 import config
 
 start = InlineKeyboardMarkup(inline_keyboard=[
-    [dict(text='🤖 Iniciar uma conversa', url='https://t.me/{}?start=start'.format(config.bot_username))]
+    [dict(text='🤖 Start a conversation', url='https://t.me/{}?start=start'.format(config.bot_username))]
 ])
 
 start_pv = InlineKeyboardMarkup(inline_keyboard=[
-    [dict(text='📚 Comandos', callback_data='all_cmds')] +
-    [dict(text='ℹ️ Informações', callback_data='infos')],
-    [dict(text='➕ Add em um grupo', url='https://t.me/{}?startgroup=new'.format(config.bot_username))] +
-    [dict(text='⭐ Avaliar', url='https://t.me/storebot?start=' + config.bot_username)]
+    [dict(text='📚 Commands', callback_data='all_cmds')] +
+    [dict(text='ℹ️ About Bot', callback_data='infos')],
+    [dict(text='➕ Add to your group', url='https://t.me/{}?startgroup=new'.format(config.bot_username))] +
+    [dict(text='⭐ Rate', url='https://t.me/storebot?start=' + config.bot_username)]
 ])
 
 all_cmds = InlineKeyboardMarkup(inline_keyboard=[
     [dict(text='👮 Admins', callback_data='admin_cmds')] +
-    [dict(text='👤 Usuários', callback_data='user_cmds')],
-    [dict(text='🔧 Ferramentas', callback_data='tools_cmds')] +
-    [dict(text='🔎 Modo inline', switch_inline_query_current_chat='/')],
-    [dict(text='« Voltar', callback_data='start_back')]
+    [dict(text='👤 Users', callback_data='user_cmds')],
+    [dict(text='🔧 Ultra Tools', callback_data='tools_cmds')] +
+    [dict(text='🔎 inline Mode', switch_inline_query_current_chat='/')],
+    [dict(text='« Go Back', callback_data='start_back')]
 ])
 
 start_back = InlineKeyboardMarkup(inline_keyboard=[
-    [dict(text='« Voltar', callback_data='start_back')]
+    [dict(text='« Go Back', callback_data='start_back')]
 ])
 
 cmds_back = InlineKeyboardMarkup(inline_keyboard=[
-    [dict(text='« Voltar', callback_data='all_cmds')]
+    [dict(text='« Go Back', callback_data='all_cmds')]
 ])
 
 del_msg = InlineKeyboardMarkup(inline_keyboard=[
-    [dict(text='🗑 Deletar mensagem', callback_data='del_msg')]
+    [dict(text='🗑 Delete All messages', callback_data='del_msg')]
 ])
 
 ia_question = InlineKeyboardMarkup(inline_keyboard=[
-    [dict(text='✅ Aceitar', callback_data='ia_yes')] +
-    [dict(text='❌ Recusar', callback_data='ia_no')]
+    [dict(text='✅ Accept', callback_data='ia_yes')] +
+    [dict(text='❌ Cancel', callback_data='ia_no')]
 ])
