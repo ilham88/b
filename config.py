1449358839222e89b8b5650891a8f879d44a1ae6
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-17 02:12:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class Config(object):
     # get a token from https://chatbase.com
     CHAT_BASE_TOKEN = os.environ["CHAT_BASE_TOKEN"]
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ["TG_BOT_TOKEN"]
+    TG_BOT_TOKEN = os.environ.get("TOKEN")
     # your domain to show when download file is greater than MAX_FILE_SIZE
     HTTP_DOMAIN = os.environ["HTTP_DOMAIN"]
     # the download location, where the HTTP Server runs
