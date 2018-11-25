67cf26ad2664823ebb513e80ca1578ed49ad045b
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-25 17:53:42 +0100

    Update DownLoadFile.py

--- plugins/DownLoadFile.py
+++ plugins/DownLoadFile.py
@@ -1,60 +1,46 @@
-#!/usr/bin/env python3
-# (c) tonyironman
-
-# the logging things
-import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-# the PTB
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, DispatcherHandlerStop, run_async
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, MessageEntity, ChatAction
-
-import subprocess
-import math
+import config
 import requests
+import dotenv
 import os
-import json
-
-from telethon import TelegramClient
-from telethon.errors import (
-    RPCError, BrokenAuthKeyError, ServerError,
-    FloodWaitError, FloodTestPhoneWaitError, FileMigrateError,
-    TypeNotFoundError, UnauthorizedError, PhoneMigrateError,
-    NetworkMigrateError, UserMigrateError, SessionPasswordNeededError
-)
-from telethon.utils import get_display_name
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-from telethon.tl.types import DocumentAttributeVideo
+import bs4 
+import re
 
 
-# the secret configuration specific things
-from config import Config
-# the Strings used for this "thing"
-from tr import Translation
 
-# the Telegram trackings
-from chatbase import Message
-ABUSIVE_SPAM = []
+bot = config.bot
+def github(msg):
+    if msg.get('text'):
+        if msg['text'].startswith('!imdb'):
+            movie_name = msg['text'][6:]
+            if msg['text'][6:] == '':
+                res = '*Uso:* `!imdb <film title>` - _Otain film information from imdb db._'
+            else:
+                remove_space = movie_name.split(' ')
+                final_name = '+'.join(remove_space)
+                page = requests.get("https://www.imdb.com/find?ref_=nv_sr_fn&q={}&s=all".format(final_name))
+                lnk = str(page.status_code)
+                soup = bs4.BeautifulSoup(page.content,'lxml', from_encoding='utf-8')
+                results = soup.findAll("td","result_text")
+                mov_title = results[0].text
+                mov_link = "http://www.imdb.com/"+results[0].a['href'] 
+                page1 = requests.get(mov_link)
+                soup = bs4.BeautifulSoup(page1.content,'lxml')
+                story_line = soup.find('div', "inline canwrap")
+                story_line = story_line.findAll("p")[0].text
+                info = soup.findAll('div', "txt-block")
+                for node in info:
+                a = node.findAll('a')
+                for i in a:
+                if "country_of_origin" in i['href']:
+                    mov_country = i.string
+                    for node in info:
+                        a = node.findAll('a')
+                        for i in a:
+                        if "primary_language" in i['href']:
+                            mov_language = i.string
+                            rating = soup.findAll('div',"ratingValue")
+                            for r in rating:
+                                mov_rating = r.strong['title']
 
-def TRChatBase(chat_id, message_text, intent):
-    msg = Message(api_key=Config.CHAT_BASE_TOKEN,
-              platform="Telegram",
-              version="1.3",
-              user_id=chat_id,
-              message=message_text,
-              intent=intent)
-    resp = msg.send()
-
-
-def DownLoadFile(url, file_name):
-    if not os.path.exists(file_name):
-        r = requests.get(url, allow_redirects=True, stream=True)
-        with open(file_name, 'wb') as fd:
-            for chunk in r.iter_content(chunk_size=4086):
-                fd.write(chunk)
-    return file_name
-
- 
-def humanbytes(size):
+                    bot.sendMessage(msg['chat']['id'], "**Title : **`{}`\n**Rating : **`{}`\n**Country : **`{}`\n**Language : **`{}`\n**IMDB Url : **`{}`\n**Story Line : **`{}`".format(mov_title, mov_rating, mov_country, mov_language, mov_link, story_line), 'Markdown', reply_to_message_id=msg['message_id'])
+                    return True
