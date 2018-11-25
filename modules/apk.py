e592cb22694a40a2c7f8d370c167565b713f228d
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-25 14:02:23 +0100

    Update apk.py

--- modules/apk.py
+++ modules/apk.py
@@ -77,26 +77,30 @@ async def _(event):
     html = requests.get(url)
     parse = BeautifulSoup(html.text)
     for i in parse.find("p"):
-        url = input_str.strip()
-        # https://stackoverflow.com/a/761825/4723940
-        file_name = app_name.strip()
-        required_file_name = TEMP_DOWNLOAD_DIRECTORY + "" + file_name
-        start = datetime.now()
-        headers = {'Accept-Language': 'en-US,en;q=0.9,te;q=0.8'}
-        r = requests.get(url, allow_redirects=True, stream=True, headers=headers)
-        with open(required_file_name, "wb") as fd:
-            total_length = r.headers.get('content-length')
-            # https://stackoverflow.com/a/15645088/4723940
-            if total_length is None: # no content length header
-                fd.write(r.content)
-            else:
-                dl = 0
-                total_length = int(total_length)
-                for chunk in r.iter_content(chunk_size=128):
-                    dl += len(chunk)
-                    fd.write(chunk)
-                    done = int(100 * dl / total_length)
-                    download_progress_string = "Downloading ... [%s%s]" % ('=' * done, ' ' * (50-done))
+        a_url = i["href"]
+        app_url = site + a_url + "/download?from=details"
+        html2 = requests.get(app_url).text
+        parse2 = BeautifulSoup(html2, features="lxml")
+        links = []
+        for link in parse2.find_all('a', {'id': 'download_link'})
+            url = input_str.strip()
+            file_name = app_name.strip()
+            required_file_name = TEMP_DOWNLOAD_DIRECTORY + "" + file_name
+            start = datetime.now()
+            headers = {'Accept-Language': 'en-US,en;q=0.9,te;q=0.8'}
+            r = requests.get(url, allow_redirects=True, stream=True, headers=headers)
+            with open(required_file_name, "wb") as fd:
+                total_length = r.headers.get('content-length')
+                if total_length is None:
+                    fd.write(r.content)
+                else:
+                    dl = 0
+                    total_length = int(total_length)
+                    for chunk in r.iter_content(chunk_size=128):
+                        dl += len(chunk)
+                        fd.write(chunk)
+                        done = int(100 * dl / total_length)
+                        download_progress_string = "Downloading ... [%s%s]" % ('=' * done, ' ' * (50-done))
                     # download_progress_string = "Downloading ... [%s of %s]" % (str(dl), str(total_length))
                     # download_progress_string = "Downloading ... [%s%s]" % ('⬛️' * done, '⬜️' * (100 - done))
                     """try:
@@ -104,7 +108,7 @@ async def _(event):
                     except MessageNotModifiedError as e:
                         print("__FLOODWAIT__: {} sleeping for 100seconds, before proceeding.".format(str(e)))
                     time.sleep(1)"""
-                    print(download_progress_string)
+                        print(download_progress_string)
         end = datetime.now()
         ms = (end - start).seconds
         await event.edit("Downloaded to `{}` in {} seconds.".format(required_file_name, ms))
