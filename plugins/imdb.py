3b2008e79d018543f82db0a071857a7784b8e899
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-25 18:02:32 +0100

    Update imdb.py

--- plugins/imdb.py
+++ plugins/imdb.py
@@ -42,4 +42,4 @@ def github(msg):
                   mov_rating = r.strong['title']
 
                 bot.sendMessage(msg['chat']['id'], "**Title : **`{}`\n**Rating : **`{}`\n**Country : **`{}`\n**Language : **`{}`\n**IMDB Url : **`{}`\n**Story Line : **`{}`".format(mov_title, mov_rating, mov_country, mov_language, mov_link, story_line), 'Markdown', reply_to_message_id=msg['message_id'])
-                    return True
+                return True
