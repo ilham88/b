fa0db6d4fba2b0cc7d94a1337e56f9e1ac9051e8
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-25 12:37:09 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 API_ID="256406"
 API_HASH="31fd969547209e7c7e23ef97b7a53c37"
-PRIVATE_CHANNEL_BOT_API_ID="USE .get_id to get from channel"              #For using .fwd command to forward a message directly without quotes to this specified channel
-PRIVATE_GROUP_BOT_API_ID="use .get_id "                #For using .fwd command to forward a message directly to a group or channel
-SCREEN_SHOT_LAYER_ACCESS_KEY="get from screenshot layer website google it "           #For using .screencapture commad...please refer readme for getting the key
-OPEN_WEATHER_MAP_APPID="get it from openweather site"        #FOR USING .weather Command
+PRIVATE_CHANNEL_BOT_API_ID="-1001139726492"
+PRIVATE_GROUP_BOT_API_ID="-1001145151462"
+SCREEN_SHOT_LAYER_ACCESS_KEY="get from screenshot layer website google it "
+OPEN_WEATHER_MAP_APPID="get it from openweather site"
 
-IBM_WATSON_CRED_USERNAME="INSERT API USERNAME" #FOR IBM WATSON AUDIO USAGE
-IBM_WATSON_CRED_PASSWORD="INSERT API PASSWORD"        #For IBM WATSON 
-LOGGER_GROUP="CHAT ID (-ve) OF THE LOG GROUP"
-LOGGER=False    #Incase you want to turn off logging, put this to false
-CONSOLE_LOGGER_VERBOSE=False
+IBM_WATSON_CRED_USERNAME="INSERT API USERNAME"
+IBM_WATSON_CRED_PASSWORD="INSERT API PASSWORD"
+LOGGER_GROUP="-1001145151462"
+LOGGER=True
+CONSOLE_LOGGER_VERBOSE=True
 PM_AUTO_BAN=False
 #DONT FILL IT YET NOT NECESSARY
 TRT_ENABLE=False
