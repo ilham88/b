2a1b8be776bda465b381e80421129196396fb0cd
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-29 00:07:32 +0100

    Update shorten.py

--- plugins/shorten.py
+++ plugins/shorten.py
@@ -1,16 +1,37 @@
 import config
 import requests
 import urllib.request
+from urllib.parse import urlparse
+import , http.client, sys
+from os.path import splitext
 bot = config.bot
 
 def shorten(msg):
     if msg.get('text'):
         if msg['text'].startswith('.trim'):
-            text = msg['text'][3:]
+            text = msg['text'][5:]
             if text == '':
-                bot.sendMessage(msg['chat']['id'], '*Uso:* `/shorten google.com` - _Encurta uma URL. Powered by_ 🇧🇷.ml', 'Markdown', reply_to_message_id=msg['message_id'])
+                bot.sendMessage(msg['chat']['id'], '*Uso:* `.trim http://google.com` - _Encurta uma URL. Powered by_ 🇧🇷.ml', 'Markdown', reply_to_message_id=msg['message_id'])
             else:
                 r = requests.get('http://trimit.gq/api?create&key=NjwzV39FqhKnumcX5gpBasObWYSZie4Adl7&link={}'.format(msg['text'][5:]))
+                r.url
+                url = r
+                url = urlparse(url)
+                conn = httplib.HTTPConnection(url.netloc)  
+                conn.request("HEAD", url.path)
+                if conn.getresponse():
+                    return True
+                return False
+            url_https = "https://" + url.split("//")[1]
+            if check_url(url_https):
+                print ("Nice, you can load it with https")
+            else:
+                if check_url(url):
+                    print ("https didn't load, but you can use http")
+                    if check_url(url):
+                        print "Nice, it does load with http too"
+                        return true
+                
                 if r.status_code != 404:
                     b = r.json()
                     print(r.json())
@@ -22,16 +43,16 @@ def shorten(msg):
                     print(c)
                     Clicks = c["Clicks"]
                     if b["Status"] != True:
-                        req = "😭 Your Link encountered a a Glitch"
+                        req = "😭 Your Link encountered a Glitch"
                         inf = ""
                         Status = b["Error"]
                         icon = "❌"
                     else:
                         req = "ℹ️ Link Details Below"
-                        inf = "(📶 Used for stats)"
+                        inf = "(Used for stats)"
                         Status = b["Status"]
                         icon = "✅"
                         
-                    bot.sendMessage(msg['chat']['id'], "\n*{}*\n\n*Trimmed Link:* {}\n\n*🆔:* `{}`  _{}_\n\n*👀 Clicks:* {}\n\n*{} Link Status:* {}".format(req, Link, ID, inf, Clicks, icon, Status), 'Markdown', reply_to_message_id=msg['message_id'])
+                    bot.sendMessage(msg['chat']['id'], "\n*{}*\n\n*Trimmed Link:* {}\n\n*🆔:* `{}`\n\n*👀 Clicks:* {}\n\n*{} Link Status:* {}".format(req, Link, ID, Clicks, icon, Status), 'Markdown', reply_to_message_id=msg['message_id'])
                 else:
                     bot.sendMessage(msg['chat']['id'], "There was an error", 'Markdown', reply_to_message_id=msg['message_id'])
