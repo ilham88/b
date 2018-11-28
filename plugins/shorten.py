e3e9563185182baf5fabc5c39d84d62ac5ab95a0
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-28 21:25:03 +0100

    Update shorten.py

--- plugins/shorten.py
+++ plugins/shorten.py
@@ -10,7 +10,6 @@ def shorten(msg):
             if text == '':
                 bot.sendMessage(msg['chat']['id'], '*Uso:* `/shorten google.com` - _Encurta uma URL. Powered by_ 🇧🇷.ml', 'Markdown', reply_to_message_id=msg['message_id'])
             else:
-                some_url = 'http://trimit.gq/api?create&key=NjwzV39FqhKnumcX5gpBasObWYSZie4Adl7&link={}'.format(msg['text'][5:])
-                filehandle = urllib.request.urlopen(some_url)
-                print(filehandle.read())
-                bot.sendMessage(msg['chat']['id'], '*Resultado:* {}'.format(filehandle.read()['Link']), 'Markdown', reply_to_message_id=msg['message_id'])
+                r = requests.get('http://trimit.gq/api?create&key=NjwzV39FqhKnumcX5gpBasObWYSZie4Adl7&link=https://github.com/Bfaschat/a/blob/master/plugins/shorten.py')
+                print(r.json())
+                bot.sendMessage(msg['chat']['id'], '*Resultado:* {}'.format(r.json()['Link']), 'Markdown', reply_to_message_id=msg['message_id'])
