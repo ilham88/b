7cd4d1b9d588469f8ce1e5cd39c6de405f0f7b1c
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-28 20:38:05 +0100

    Update shorten.py

--- plugins/shorten.py
+++ plugins/shorten.py
@@ -5,11 +5,11 @@ bot = config.bot
 
 def shorten(msg):
     if msg.get('text'):
-        if msg['text'].startswith('/shorten'):
-            text = msg['text'][9:]
+        if msg['text'].startswith('!sh'):
+            text = msg['text'][3:]
             if text == '':
                 bot.sendMessage(msg['chat']['id'], '*Uso:* `/shorten google.com` - _Encurta uma URL. Powered by_ 🇧🇷.ml', 'Markdown', reply_to_message_id=msg['message_id'])
             else:
-                r = requests.post('https://amanosite.000webhostapp.com/api/', data=dict(url=text))
-                bot.sendMessage(msg['chat']['id'], '*Resultado:* {}'.format(r.json()['result']), 'Markdown',
+                r = requests.post('http://trimit.gq/api?create&key=NjwzV39FqhKnumcX5gpBasObWYSZie4Adl7&', data=dict(link=text))
+                bot.sendMessage(msg['chat']['id'], '*Resultado:* {}'.format(r.json()['Link']), 'Markdown',
                                 reply_to_message_id=msg['message_id'])
