b37a1bb79cd400f2327393d53fd84272d19bf2c7
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-28 21:50:10 +0100

    Update shorten.py

--- plugins/shorten.py
+++ plugins/shorten.py
@@ -8,8 +8,20 @@ def shorten(msg):
         if msg['text'].startswith('.trim'):
             text = msg['text'][3:]
             if text == '':
-                bot.sendMessage(msg['chat']['id'], '*Uso:* `/shorten google.com` - _Encurta uma URL. Powered by_ 🇧🇷.ml', 'Markdown', reply_to_message_id=msg['message_id'])
+                res = bot.sendMessage(msg['chat']['id'], '*Uso:* `/shorten google.com` - _Encurta uma URL. Powered by_ 🇧🇷.ml', 'Markdown', reply_to_message_id=msg['message_id'])
             else:
                 r = requests.get('http://trimit.gq/api?create&key=NjwzV39FqhKnumcX5gpBasObWYSZie4Adl7&link={}'.format(msg['text'][5:]))
-                print(r.json())
-                bot.sendMessage(msg['chat']['id'], '*Resultado:* {}'.format(r.json()['Link']), 'Markdown', reply_to_message_id=msg['message_id'])
+                if r.status_code != 404:
+                    b = r.json()
+                    print(r.json())
+                    Link = b["Link"]
+                    ID = b["ID"]
+                    Error = b["Error"]
+                    Status = b["Status"]
+                    res = """*Original Link:* [Click Here ℹ️]({})
+*Trimmed Link:* {}
+*🆔:* `{}`
+*❌ Error:* {}
+*✅ Link Status:* {}""".format(text, Link, Error, Status)
+                    bot.sendMessage(msg['chat']['id'], res, 'Markdown', reply_to_message_id=msg['message_id'])
+                    retrun true
