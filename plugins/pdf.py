0c3519ffd2aa1be6b3d13e1d0a5a5dcc72771bd6
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-29 10:55:51 +0100

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -16,19 +16,12 @@ def pdf(msg):
             query = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"
             headers={"X-Mashape-Key": "kAvkvpaPUJmshT7QBh0JDUC35d5Jp137h8djsn7GvDlBT3Gj8K", "Content-Type": "application/x-www-form-urlencoded", "Accept": "application/json"}
             r = requests.get(query, headers=headers)
-            json_obj = urllib.request.urlopen(query).read
-            player_json_list = json.loads(json_obj)
-            for player in readable_json_list:
-                print (player['quote'])
-                print (player['author'])
-                print (player['category'])
-                print (player)
             if r.status_code != 404:
                 b = r.json()
                 print(b)
                 qoute = b[0]["quote"]
-                author = b["author"]
-                category = b["category"]
+                author = b[1]["author"]
+                category = b[2]["category"]
                 req = " Popular Quote"
                 icon = "💬"
             bot.sendMessage(msg['chat']['id'], "\n\n{} *{}*\n\n*Author:* `{}`\n\n*Category:* `{}`\n\n*{} Content:* {}".format(icon, req, author, category, q), 
