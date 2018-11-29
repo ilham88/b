61cd820b9baadf3fdfa02b2df6fc1da9f2f70a33
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-29 11:19:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,27 +12,24 @@ MESSAGE = "{} Welcome to [Pyrogram](https://docs.pyrogram.ml/)'s group chat {}!"
 app = Client("671045549:AAEN2iA-4VXECyuHhyC1TPrRLDVXLRKGS1o")
 
     
-
-
 @app.on_message(Filters.text & Filters.chat("Bfas237group"))
-def move(client, message):
+async def move(client, message):
     if "Oft" in message.text:
-       client.send_message("bfas237off", "`{}` wrote:\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.text))
-        client.send_message("Bfas237group", "I moved this discussion to the [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id)
-        client.delete_messages(
+        await client.send_message("bfas237off", "`{}` wrote:\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.text))
+        await client.send_message("Bfas237group", "I moved this discussion to the [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id)
+        await client.delete_messages(
     "Bfas237group",
     message_ids=message.message_id
 )
 
 @app.on_message(Filters.text & Filters.chat("bfas237off"))
-def move(client, message):
+async def move(client, message):
     if "Ont" in message.text:
-        client.send_message("Bfas237group", "`{}` wrote:\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.text))
-        client.send_message("bfas237off", "This happens to be an [OnTopic Discussion ↗️](https://t.me/Bfas237group/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id)
-        client.delete_messages(
+        await client.send_message("Bfas237group", "`{}` wrote:\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.text))
+        await client.send_message("bfas237off", "This happens to be an [OnTopic Discussion ↗️](https://t.me/Bfas237group/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id)
+        await client.delete_messages(
     "bfas237off",
     message_ids=message.message_id
 )
-
 
 app.run()  # Automatically start() and idle()
