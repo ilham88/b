33a23e2e995e01f1b9a0e00e6dceb2ed93163827
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-29 10:52:46 +0100

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -16,7 +16,7 @@ def pdf(msg):
             query = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"
             headers={"X-Mashape-Key": "kAvkvpaPUJmshT7QBh0JDUC35d5Jp137h8djsn7GvDlBT3Gj8K", "Content-Type": "application/x-www-form-urlencoded", "Accept": "application/json"}
             r = requests.get(query, headers=headers)
-            json_obj = urllib2.urlopen(query).read
+            json_obj = urllib.request.urlopen(query).read
             player_json_list = json.loads(json_obj)
             for player in readable_json_list:
                 print (player['quote'])
