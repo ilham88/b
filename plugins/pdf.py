b0200cc552e2d21b81b5a52bfa9ce114b0cff03b
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-29 10:28:19 +0100

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -13,23 +13,21 @@ from tldextract import extract
 def pdf(msg):
     if msg.get('text'):
         if msg['text'].startswith('.quote'):
-            text = msg['text'][6:]
+            text = msg['text'][5:]
             if text == '':
-                bot.sendMessage(msg['chat']['id'], '*Uso:* `.trim http://google.com` - _Encurta uma URL. Powered by_ 🇧🇷.ml', 'Markdown', reply_to_message_id=msg['message_id'])
+                bot.sendMessage(msg['chat']['id'], '*Use:* `.quote` - _Get Popular Quotes by famous people_', 'Markdown', reply_to_message_id=msg['message_id'])
             else:
-                url = "http://trimit.gq/api"
-                remove_spacec = url.split(' ')
-                final_namec = ''.join(remove_spacec)
                 query = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"
                 headers={"X-Mashape-Key": "kAvkvpaPUJmshT7QBh0JDUC35d5Jp137h8djsn7GvDlBT3Gj8K", "Content-Type": "application/x-www-form-urlencoded", "Accept": "application/json"}
                 r = requests.get(query, headers=headers)
                 if r.status_code != 404:
                         b = r.json()
                         print(b)
-                        extractedDomain = tldextract.extract(final_namec)
-                        domainSuffix = extractedDomain.domain + '.' + extractedDomain.suffix
-                        print(domainSuffix)    
-                        dlb = InlineKeyboardMarkup(inline_keyboard=[[dict(text='↗️ Visit', url='https://ddhdhd.deddd')]])
-                        bot.sendMessage(msg['chat']['id'], "This is just a test", 
-                            parse_mode='Markdown', reply_to_message_id=msg['message_id'], reply_markup=dlb)
+                        quote = b["quote"]
+                        author = b["author"]
+                        category = b["category"]
+                        req = " Popular Quote"
+                        icon = "💬"
+                        bot.sendMessage(msg['chat']['id'], "\n\n{} *{}*\n\n*Author:* `{}`\n\n*Category:* `{}`\n\n*{} Content:* {}".format(icon, req, author, category, quote), 
+                            parse_mode='Markdown', reply_to_message_id=msg['message_id'])
 
