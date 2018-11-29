35741f10157bf55358030b7b72d752910d87e28c
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-29 10:06:18 +0100

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -1,48 +1,34 @@
 import config
 import requests
-import dotenv
-import os
-import bs4 
-import re
-from datetime import datetime
-from urllib.parse import urlparse
-from os.path import splitext
-from urllib.request import urlretrieve
-from urllib.request import urlopen
-from shutil import copyfileobj
-from tempfile import NamedTemporaryFile
-import threading
-import pprint
-import traceback
 import urllib.request
-from urllib import request as urlrequest
-try:
-    import urllib.request
-    python3 = True
-except ImportError:
-    import urllib2
-    python3 = False
-
-
+from urllib.parse import urlparse, urlsplit
+import http.client, sys, re
+from os.path import splitext
 bot = config.bot
-def pdf(msg):
+import tldextract
+from amanobot.namedtuple import InlineKeyboardMarkup
+from amanobot.exception import TelegramError, NotEnoughRightsError
+import keyboard
+from tldextract import extract
+def shorten(msg):
     if msg.get('text'):
-        if msg['text'].startswith('.q'):
-            movie_name = msg['text'][2:]
-            if msg['text'][2:] == '':
-                res = '*Uso:* `.q <film title>` - _Otain film information from imdb db._'
+        if msg['text'].startswith('.quote'):
+            text = msg['text'][6:]
+            if text == '':
+                bot.sendMessage(msg['chat']['id'], '*Uso:* `.trim http://google.com` - _Encurta uma URL. Powered by_ 🇧🇷.ml', 'Markdown', reply_to_message_id=msg['message_id'])
             else:
-                remove_space = movie_name.split(' ')
-                final_name = '+'.join(remove_space)
-                query = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"
-                r = urlrequest.Request(query,data=None,headers={
-    "X-Mashape-Key": "kAvkvpaPUJmshT7QBh0JDUC35d5Jp137h8djsn7GvDlBT3Gj8K",
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Accept": "application/json"
-  })
-                print (r.body)
-                t=r.body
-                bot.sendMessage(msg['chat']['id'], t['quote'], 'Markdown', reply_to_message_id=msg['message_id'])
-               
-                return
-                
+                text = text.replace("http://","")
+                text = text.replace("https://","")
+                if not re.match(r'http(s?)\:', text):
+                    url = 'http://' + text
+                    parsed = urlsplit(url)
+                    host = parsed.netloc
+                    if host.startswith('www.'):
+                        host = host[4:]
+                    remove_spacec = url.split(' ')
+                    final_namec = ''.join(remove_spacec)
+                    r = requests.get('http://trimit.gq/api?create&key=NjwzV39FqhKnumcX5gpBasObWYSZie4Adl7&link={}'.format(final_namec))
+                    query = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"
+                    rs = urlrequest.Request(query,data=None,headers={"X-Mashape-Key": "kAvkvpaPUJmshT7QBh0JDUC35d5Jp137h8djsn7GvDlBT3Gj8K", "Content-Type": "application/x-www-form-urlencoded", "Accept": "application/json"})
+                    print(rs)
+                 
