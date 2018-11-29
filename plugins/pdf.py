92a3485d2c49bbeba8892a57e5ad1c600688370e
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-29 10:33:36 +0100

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -12,22 +12,18 @@ import keyboard
 from tldextract import extract
 def pdf(msg):
     if msg.get('text'):
-        if msg['text'].startswith('.quote'):
-            text = msg['text'][5:]
-            if text == '':
-                bot.sendMessage(msg['chat']['id'], '*Use:* `.quote` - _Get Popular Quotes by famous people_', 'Markdown', reply_to_message_id=msg['message_id'])
-            else:
-                query = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"
-                headers={"X-Mashape-Key": "kAvkvpaPUJmshT7QBh0JDUC35d5Jp137h8djsn7GvDlBT3Gj8K", "Content-Type": "application/x-www-form-urlencoded", "Accept": "application/json"}
-                r = requests.get(query, headers=headers)
-                if r.status_code != 404:
-                        b = r.json()
-                        print(b)
-                        quote = b["quote"]
-                        author = b["author"]
-                        category = b["category"]
-                        req = " Popular Quote"
-                        icon = "💬"
-                        bot.sendMessage(msg['chat']['id'], "\n\n{} *{}*\n\n*Author:* `{}`\n\n*Category:* `{}`\n\n*{} Content:* {}".format(icon, req, author, category, quote), 
+        if msg['text'].split()[0] == '!q':
+            query = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous"
+            headers={"X-Mashape-Key": "kAvkvpaPUJmshT7QBh0JDUC35d5Jp137h8djsn7GvDlBT3Gj8K", "Content-Type": "application/x-www-form-urlencoded", "Accept": "application/json"}
+            r = requests.get(query, headers=headers)
+            if r.status_code != 404:
+                b = r.json()
+                print(b)
+                quote = b["quote"]
+                author = b["author"]
+                category = b["category"]
+                req = " Popular Quote"
+                icon = "💬"
+            bot.sendMessage(msg['chat']['id'], "\n\n{} *{}*\n\n*Author:* `{}`\n\n*Category:* `{}`\n\n*{} Content:* {}".format(icon, req, author, category, quote), 
                             parse_mode='Markdown', reply_to_message_id=msg['message_id'])
 
