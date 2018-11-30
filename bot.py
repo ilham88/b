1d39781ed8009a9ca6bed6379fbdde4efaffd30c
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-11-30 15:28:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,33 @@
-"""This is the Welcome Bot in @PyrogramChat.
+from pyrogram import Client, Filters
 
-It uses the Emoji module to easily add emojis in your text messages and Filters
-to make it only work for specific messages in a specific chat.
-"""
-
-from pyrogram import Client, Emoji, Filters
-
-MENTION = "[{}](tg://user?id={})"
-MESSAGE = "{} Welcome to [Pyrogram](https://docs.pyrogram.ml/)'s group chat {}!"
+app = Client(os.environ.get('TOKEN'))
 
 
-    
 @app.on_message(Filters.text & Filters.chat("Bfas237group"))
-async def move(client, message):
-    if "Oft" in message.text:
-        await client.send_message("bfas237off", "`{}` wrote:\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.text))
-        await client.send_message("Bfas237group", "I moved this discussion to the [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id)
-        await client.delete_messages(
+def move(client, message):
+  if "Oft" in message.text:
+      client.send_message("bfas237off", "`{}` wrote:\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.text))
+      message.reply("I moved this discussion to the [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id, quote=True)
+      client.delete_messages(
     "Bfas237group",
     message_ids=message.message_id
 )
-
+      client.delete_messages(
+    "bfas237off",
+    message_ids=message.reply_to_message.message_id
+)
 @app.on_message(Filters.text & Filters.chat("bfas237off"))
-async def move(client, message):
+def move(client, message):
     if "Ont" in message.text:
-        await client.send_message("Bfas237group", "`{}` wrote:\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.text))
-        await client.send_message("bfas237off", "This happens to be an [OnTopic Discussion ↗️](https://t.me/Bfas237group)", reply_to_message_id=message.reply_to_message.message_id)
-        await client.delete_messages(
+        client.send_message("Bfas237group", "`{}` wrote:\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.text))
+        client.send_message("bfas237off", "This happens to be an [OnTopic Discussion ↗️](https://t.me/Bfas237group)", reply_to_message_id=message.reply_to_message.message_id)
+        client.delete_messages(
     "bfas237off",
     message_ids=message.message_id
+)
+        client.delete_messages(
+    "bfas237off",
+    message_ids=message.reply_to_message.message_id
 )
 
 app.run()  # Automatically start() and idle()
