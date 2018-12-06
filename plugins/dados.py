8dc1ce56bdeaf77774a1c812da5093b94179c5fd
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-06 22:06:50 +0100

    Update dados.py

--- plugins/dados.py
+++ plugins/dados.py
@@ -127,15 +127,15 @@ def dados(msg):
             if len(APPS) > 5:
                 x = ""
                 for idx, app in enumerate(APPS):
-                    apps = "_".join(app[0].split())
+                    apps = app[0].replace(" ","_")
                     x += """`[{:02d}]` *{}*\n /{}\n\n""".format(idx, app[0], apps)
                 bot.editMessageText((msg['chat']['id'], sent), "⬆️ Uploading to Telegram \n\n {}".format(x), 'Markdown')
-                
+                markup = ForceReply()
+                bot.sendMessage(msg['chat']['id'], "Which app would you like to download?", 'Markdown', reply_to_message_id=msg['message_id'], reply_markup=markup)['message_id']
                 
                 option = ""
                 while option == "":
-                    markup = ForceReply()
-                bot.sendMessage(msg['chat']['id'], "Which app would you like to download?", 'Markdown', reply_to_message_id=msg['message_id'], reply_markup=markup)['message_id']
+
                     option = input(msg['text'][2:])
                     try:
                         if 0 <= int(msg['text'][2:]) < len(APPS):
