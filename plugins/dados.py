d995a94d1b130d07cb1f7281db5b720800525a97
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-06 21:27:55 +0100

    Update dados.py

--- plugins/dados.py
+++ plugins/dados.py
@@ -128,11 +128,11 @@ def dados(msg):
             if len(APPS) > 5:
                 x = ""
                 for idx, app in enumerate(APPS):
-                    x += """[{:02d}] *{}*\n *Developer*: _{}_\n    ================== \n""".format(idx, app[0], app[1])
+                    x += """[{:02d}] {}""".format(idx, app[0])
                 bot.editMessageText((msg['chat']['id'], sent), "⬆️ Uploading to Telegram \n\n {}".format(x), 'Markdown')
                 markup = ForceReply()
-                bot.sendMessage(msg['chat']['id'], 'Which app do you want to download', reply_to_message_id=msg['message_id'], reply_markup=markup)
-                inpu = msg['text'][2:]
+                bot.sendMessage(msg['chat']['id'], "🔁 getting download link, reply_to_message_id=msg['message_id'], reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="""[{:02d}] {}""".format(idx, app[0]))]]))['message_id']
+                
                 bot.sendMessage(msg['chat']['id'], 'Downloading {}.apk ...'.format(APPS[inpu][2].split('/')[-1]), 'Markdown')
                         
                 
