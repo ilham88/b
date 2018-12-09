9a64f817885b57f60777e5bfa6dde83920c1de6f
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-09 10:06:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,62 @@
-"""This is the Welcome Bot in @PyrogramChat.
-
-It uses the Emoji module to easily add emojis in your text messages and Filters
-to make it only work for specific messages in a specific chat.
-"""
-
-from pyrogram import Client, Emoji, Filters
-
-MENTION = "[{}](tg://user?id={})"
-MESSAGE = "{} Welcome to [Pyrogram](https://docs.pyrogram.ml/)'s group chat {}!"
+# init project
+from flask import Flask, jsonify, render_template, request
+from os import environ
+application = Flask(__name__)
 
 
-    
+# I've started you off with Flask, 
+# but feel free to use whatever libs or frameworks you'd like through `.requirements.txt`.
+
+# unlike express, static files are automatic: http://flask.pocoo.org/docs/0.12/quickstart/#static-files
+
+# http://flask.pocoo.org/docs/0.12/quickstart/#routing
+# http://flask.pocoo.org/docs/0.12/quickstart/#rendering-templates
+@application.route('/')
+def hello():
+    return render_template('index.html')
+
+
+from pyrogram import Client, Filters
+app = Client(environ['TOKEN'])
+
+
+
 @app.on_message(Filters.text & Filters.chat("Bfas237group"))
-async def move(client, message):
-    if "Oft" in message.text:
-        await client.send_message("bfas237off", "`{}` wrote:\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.text))
-        await client.send_message("Bfas237group", "I moved this discussion to the [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id)
-        await client.delete_messages(
+def move(client, message):
+  if "Oft" in message.text:
+      client.send_message("bfas237off", "[{}](tg://user?id={}) **wrote:**\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.from_user.id, message.reply_to_message.text))
+      message.reply("I moved this discussion to the [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id, quote=True)
+      client.delete_messages(
     "Bfas237group",
     message_ids=message.message_id
 )
-
+      client.delete_messages(
+    "bfas237off",
+    message_ids=message.reply_to_message.message_id
+)
 @app.on_message(Filters.text & Filters.chat("bfas237off"))
-async def move(client, message):
+def move(client, message):
     if "Ont" in message.text:
-        await client.send_message("Bfas237group", "`{}` wrote:\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.text))
-        await client.send_message("bfas237off", "This happens to be an [OnTopic Discussion ↗️](https://t.me/Bfas237group/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id)
-        await client.delete_messages(
+        client.send_message("Bfas237group", "[{}](tg://user?id={}) **wrote:**\n{}\n\n**⬇️ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛɪɴᴜᴇ ʜᴇʀᴇ ⬇️**".format(message.reply_to_message.from_user.first_name, message.reply_to_message.from_user.id, message.reply_to_message.text))
+        message.reply("The Main group has been created for this discussion so why not join [Offtopic Group ↗️](https://t.me/bfas237off/{})".format(message.reply_to_message.message_id, message.reply_to_message.text), reply_to_message_id=message.reply_to_message.message_id, quote=True)
+        client.delete_messages(
     "bfas237off",
     message_ids=message.message_id
 )
-
+        client.delete_messages(
+    "Bfas237group",
+    message_ids=message.reply_to_message.message_id
+)
+ # Automatically start() and idle()
 @app.on_message(Filters.private & Filters.document)
-async def _(c, m):
-    await r = c.send_message(
+def _(c, m):
+    r = c.send_message(
         "bfas237off",
         "I'm downloading this ^",
         reply_to_message_id=message.reply_to_message.message_id
     )
     
-    await c.download_media(
+    c.download_media(
         m,
         progress=p,
         progress_args=(r.message_id,)
@@ -48,14 +65,14 @@ async def _(c, m):
 last_progress = 0
 
 
-await def p(c, cur, tot, message_id):
+def p(c, cur, tot, message_id):
     global last_progress
 
     progress = cur * 100 // tot
 
     if progress != last_progress:
         try:
-            await c.edit_message_text(
+            c.edit_message_text(
                 "bfas237off",
                 message_id,
                 "**Downloading**: `{}%`".format(progress)
@@ -64,6 +81,26 @@ await def p(c, cur, tot, message_id):
             last_progress = progress
         except:
             pass
+# Simple in-memory store
+dreams = [
+  'Find and count some sheep',
+  'Climb a really tall mountain',
+  'Wash the dishes',
+]
 
+@application.route('/dreams', methods=['GET'])
+def get_dreams():
+    return jsonify(dreams)
 
-app.run()  # Automatically start() and idle()
+# could also use the POST body instead of query string: http://flask.pocoo.org/docs/0.12/quickstart/#the-request-object
+@application.route('/dreams', methods=['POST'])
+def add_dream():
+    dreams.append(request.args.get('dream'))
+    return ''
+  
+  
+# listen for requests :)
+if __name__ == "__main__":
+    app.run() 
+    from os import environ
+    application.run(host='0.0.0.0', port=int(environ['PORT']))
