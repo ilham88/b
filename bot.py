5b2298fe1b435a96b2688973d36e05163c392a06
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-09 10:27:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,5 @@
-# init project
-from flask import Flask, jsonify, render_template, request
+
 from os import environ
-application = Flask(__name__)
-
-
-# I've started you off with Flask, 
-# but feel free to use whatever libs or frameworks you'd like through `.requirements.txt`.
-
-# unlike express, static files are automatic: http://flask.pocoo.org/docs/0.12/quickstart/#static-files
-
-# http://flask.pocoo.org/docs/0.12/quickstart/#routing
-# http://flask.pocoo.org/docs/0.12/quickstart/#rendering-templates
-@application.route('/')
-def hello():
-    return render_template('index.html')
-
-
 from pyrogram import Client, Filters
 app = Client(environ['TOKEN'])
 
@@ -81,26 +65,6 @@ def p(c, cur, tot, message_id):
             last_progress = progress
         except:
             pass
-# Simple in-memory store
-dreams = [
-  'Find and count some sheep',
-  'Climb a really tall mountain',
-  'Wash the dishes',
-]
-
-@application.route('/dreams', methods=['GET'])
-def get_dreams():
-    return jsonify(dreams)
-
-# could also use the POST body instead of query string: http://flask.pocoo.org/docs/0.12/quickstart/#the-request-object
-@application.route('/dreams', methods=['POST'])
-def add_dream():
-    dreams.append(request.args.get('dream'))
-    return ''
-  
-  
-# listen for requests :)
-if __name__ == "__main__":
-    app.run() 
-    from os import environ
-    application.run(host='0.0.0.0', port=int(environ['PORT']))
+        
+app.run() 
+    
