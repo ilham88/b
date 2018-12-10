bf8dea35d3873e17f2d3134f8194823286fc0615
Ningmua Bruno <gwatanajohn3@gmail.com>
2018-12-10 19:05:43 +0100

    Update dados.py

--- plugins/dados.py
+++ plugins/dados.py
@@ -160,7 +160,8 @@ async def handler(event):
     await message.edit('Download Ended!')    
     await asyncio.sleep(5)
     await bot.send_file("bfas237off", filename, reply_to=event.id, caption="`Here is your current status`")
-    os.remove(file)
+    os.remove(filename)
+    os.remove(required_file_name)
     
     
 
